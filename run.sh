e439118d65a9967819987c7a74199ecd867a12d4
Christoph Dittmann <github@yozora.eu>
2026-01-20 22:20:29 +0100

    Inline run-nethack.sh in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-tmux new-session -d -s nethack -n nethack sh/run-nethack.sh robot \; \
+tmux new-session -d -s nethack -n nethack bash -c '
+while true; do
+    NETHACKOPTIONS="role:wiz,race:hum,align:neu,gender:mal,!autopickup" \
+      ./nethack-llm/src/nethack -u robot || exit
+    echo "NetHack exited. Restarting in 2 seconds..."
+    sleep 2 || exit
+done
+' \; \
   set-window-option -t nethack window-size manual \; \
   resize-pane -t 0 -x 80 -y 24 \; \
   attach -t nethack
--- sh/run-nethack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-    NETHACKOPTIONS='role:wiz,race:hum,align:neu,gender:mal,!autopickup' \
-      ./nethack-llm/src/nethack -u "$1" || exit
-    echo "NetHack exited. Restarting in 2 seconds..."
-    sleep 2
-done
