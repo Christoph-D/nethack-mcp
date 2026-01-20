61f60b1eef4672b11bce8c3d2b9f781299d5efa5
Christoph Dittmann <github@yozora.eu>
2026-01-20 22:10:55 +0100

    Disable autopickup

--- sh/run-nethack.sh
+++ sh/run-nethack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    NETHACKOPTIONS=role:wiz,race:hum,align:neu,gender:mal \
+    NETHACKOPTIONS='role:wiz,race:hum,align:neu,gender:mal,!autopickup' \
       ./nethack-llm/src/nethack -u "$1" || exit
     echo "NetHack exited. Restarting in 2 seconds..."
     sleep 2
